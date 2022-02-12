df7d89bcf1ba0013f39782480d122c604756d5e2
유국현 <rnrgus5897@gmail.com>
2022-02-12 18:35:53 +0900

    settimeout 1초로

--- router/index.js
+++ router/index.js
@@ -32,34 +32,6 @@ router.get('/' , async(req, res) => {
     }
 })
 
-router.get("/vote" , async(req , res) => {
-
-    try{
-        let voteData
-
-        let voteList = "";
-            voteList += "select concat(round(( sum(vote)/100 * 100 ) , 0) , '%') as percentage, "
-            voteList += "sum(people) as people , "
-            voteList += "sum(vote) as vote , "
-            voteList += "name as name "
-            voteList += "from exampleVote where name in ('이재명' , '윤석열')  group by name "   
-
-            let params = [];
-            await conn.query(voteList , params , async (err , data) => {
-                if(err) console.log("Error" , err)
-                voteData = await data
-
-                console.log("voteData" , voteData)
-
-                res.render('index' , {voteData})
-            })
-      
-    }catch(err){
-        console.error("Error" , err)
-    }
-})
-
-
 router.post('/insertVote' , async(req , res) => {
     console.log(req.body)
     let result = {}
